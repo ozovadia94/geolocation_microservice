15858de602502a09454eff23acf90af4e7568e26
ozovadia94 <ozovad@gmail.com>
2022-06-16 10:13:36 +0300

    Improving the code and efficiency

--- router-controller.js
+++ router-controller.js
@@ -6,12 +6,14 @@ const getHello = (req, res) => {
     res.status(200).send()
 }
 
-const QueryUpperCase = async (query)=>{
+const QueryUpperCase = async (req,res,query) => {
     let { source, destination } = query
+
     if (!source || !destination) {
+        res.status(400).send({"message": "Missing fields"})
         return null;
     }
-    
+
     source = source.toUpperCase()
     destination = destination.toUpperCase()
     if (source > destination) {
@@ -19,61 +21,73 @@ const QueryUpperCase = async (query)=>{
         destination = source
         source = temp
     }
-    else if(source===destination)
+    else if (source === destination)
+    {
+        res.status(400).send({"message": "Equal source and destination"})
         return null
-    return {source,destination}
+    }
+        
+    return { source, destination }
 }
 
 const getDistance = async (req, res) => {
-    let query = await QueryUpperCase(req.query)
+    let query = await QueryUpperCase(req,res,req.query)
 
-    if (query===null) {
-        res.status(500).send()
-        return
+    if (query === null) {
+        return;
     }
     const { source, destination } = query
 
 
     const find = await Distance.findOneAndUpdate({ "source": source, "destination": destination },
-        { $inc: { "hits": 1 } }).then(result => {
-            return result
-        }).catch((e) => {
+        { $inc: { "hits": 1 } })
+        .catch((e) => {
             console.log("Dberror")
+            res.status(500).send({ message: "DB problem"})
+            return;
         })
 
-    if (find !== null && find.distance) {
+    if (find && find.distance) {
         res.status(200).send({ "distance": find.distance })
         return;
     }
 
     const url = 'https://maps.googleapis.com/maps/api/distancematrix/json?' + `origins=${source}&destinations=${destination}&key=${process.env.GoogleAPI}`
-    axios.get(url).then((result => {
-        let distance = result.data.rows[0].elements[0].distance['value'] / 1000
+    axios.get(url)
+        .then((result => {
+            if (result.data.rows[0].elements[0].status === 'OK') {
+                let distance = result.data.rows[0].elements[0].distance['value'] / 1000
 
-        const distanceOb = new Distance({
-            source: source, //
-            destination: destination,
-            distance: distance,
-            hits: 1,
+                const distanceOb = new Distance({
+                    source: source, //
+                    destination: destination,
+                    distance: distance,
+                    hits: 1,
+                })
+
+                distanceOb.save().then((result) => {
+                    console.log("add. (from api)")
+                    res.status(200).send({ distance: distance })
+
+                }).catch((e) => {
+                    // console.log(e)
+                    res.status(500).send(e)
+                })
+            }
+            else
+                res.status(404).send({ "message": "Not Found" })
+
+
+
+        })).catch((e) => {
+            console.log("Can't find distance")
+            res.status(404).send({"message": "Can't find distance"})
         })
-
-        distanceOb.save().then((result) => {
-            console.log("add. (from api)")
-
-        }).catch((e) => {
-            console.log(e)
-            res.status(500).send(e)
-        })
-
-        res.status(200).send({ distance: distance })
-    })).catch((e) => {
-        console.log("Can't find distance")
-        res.status(404).send()
-    })
 }
-const getHealth = (req, res) => {
+const getHealth =  (req, res) => {
     try {
-        const code = mongoose.connection.readyState
+        const code =  mongoose.connection.readyState
+        console.log(code)
         if (code == 1) {
             res.status(200).send()//ok
             return;
@@ -86,54 +100,35 @@ const getHealth = (req, res) => {
 }
 
 const getPopularsearch = async (req, res) => {
-    await Distance.find().then((result) => {
-        let max_val = 0;
-        let max_index = result[0]
-        for (let key in result) {
-            if (result[key].hits > max_val) {
-                max_val = result[key].hits
-                max_index = key
-            }
-        }
-        res.status(200).send({ "source": result[max_index].source, "destination": result[max_index].destination, "hits": result[max_index].hits })
+    await Distance.find().sort({ hits: -1 }).limit(1).then((result) => {
+        res.status(200).send({ "source": result[0].source, "destination": result[0].destination, "hits": result[0].hits })
     }).catch((e) => {
         console.log(e)
-        res.status(404).send()
+        res.status(404).send({"message": "Not found"})
         return;
     })
 
 }
 
 const postDistance = async (req, res) => {
-    let query = await QueryUpperCase(req.body)
+    let query = await QueryUpperCase(req,res,req.body)
 
-    if (query===null) {
-        res.status(500).send()
+    if (query === null) {
         return
     }
     const { source, destination } = query
 
-    var {distance } = req.body
+    const { distance } = req.body
     let hits = 1;
 
     if (!distance || distance < 0)//Check for a missing title
     {
-        res.status(500).send()
+        res.status(400).send({"message": "Missing fields"})
         return;
     }
-    source.toUpperCase()
-    destination.toUpperCase()
 
-    if (source > destination) {
-        let temp = destination
-        destination = source
-        source = temp
-    }
-
-    var x = await Distance.findOneAndDelete({ "source": source, "destination": destination })
-        .then(result => {
-            return result
-        })
+    const x = await Distance.findOneAndDelete({ "source": source, "destination": destination })
+        
     if (x !== null) {
         hits = x.hits
     }
@@ -147,7 +142,6 @@ const postDistance = async (req, res) => {
 
     distanceOb.save().then((result) => {
         res.status(201).send({ "source": result.source, "destination": result.destination, "hits": result.hits })
-        // res.status(201).send()
 
     }).catch((e) => {
         console.log(e)
